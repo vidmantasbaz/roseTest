89f4075584c00ef5e03fbc1ed1634b05b5e09f16
vidmantasbazevicius <vidmantas.bazevicius@transfergo.com>
2020-06-28 01:21:00 +0300

    update Type  fix misspelling

--- src/Type.php
+++ src/Type.php
@@ -1,19 +1,19 @@
 <?php
 
-
 namespace App;
+
 class Type
 {
     public const AGED_BRIE = 'Aged Brie';
     public const SULFURAS = 'Sulfuras';
-    public const BACKSTAG4E_PASS = 'Backstage passes';
+    public const BACKSTAGE_PASS = 'Backstage passes';
     public const CONJURED = 'Conjured';
     public const OTHER = 'other';
 
     public static $map = [
         self::AGED_BRIE,
         self::SULFURAS,
-        self::BACKSTAG4E_PASS,
+        self::BACKSTAGE_PASS,
         self::CONJURED
     ];
 }
