eab06c357884f3882677589f4323d85de3cd58eb
vidmantasbazevicius <vidmantas.bazevicius@transfergo.com>
2020-06-28 01:22:34 +0300

    Updated main function to use  service for calculation

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -1,61 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
+
+use App\Service\GoodsService;
 
 final class GildedRose {
 
+    /**
+     * @var Item[]
+     */
     private $items = [];
 
-    public function __construct($items) {
+    /**
+     * @var GoodsService
+     */
+    private $goodsService;
+
+    public function __construct(array $items, GoodsService $goodsService) {
         $this->items = $items;
+        $this->goodsService = $goodsService;
     }
 
     public function updateQuality() {
+
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-            
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+            $goods = $this->goodsService->determineType(new GoodsDTO($item));
+            switch ($goods->getType()) {
+                case Type::BACKSTAGE_PASS:
+                    $this->goodsService->updateBackstagePassesGoods($goods);
+                    break;
+                case Type::SULFURAS:
+                    break;
+                case Type::AGED_BRIE:
+                    $this->goodsService->updateAgedBrieGoods($goods);
+                    break;
+                case Type::CONJURED:
+                    $this->goodsService->updateConjuredGoods($goods);
+                    break;
+                default:
+                    $this->goodsService->updateOtherGoods($goods);
             }
         }
     }
